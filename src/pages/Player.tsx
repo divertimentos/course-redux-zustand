1263d2edfa56d0ed1c72b9ff95ed533043cb9cd3
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-18 20:30:30 -0300

    WIP

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,18 +1,13 @@
 import { ChevronDown, MessageCircle, Video } from "lucide-react";
 import ReactPlayer from "react-player";
+import { Header } from "../components/Header";
 
 export function Player() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
         <div className="flex items-center justify-between">
-          {/* Header */}
-          <div className="flex flex-col gap-1">
-            <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-            <span className="text-sm text-zinc-400">
-              Módulo "Desvendando o Redux"
-            </span>
-          </div>
+          <Header />
 
           <button className="flex items-center gap-2 rounded bg-violet-500 hover:bg-violet-600 px-3 py-2 text-sm font-medium text-white">
             <MessageCircle className="w-4 h-4" />
@@ -21,14 +16,6 @@ export function Player() {
         </div>
         <main className="relative flex overflow-hidden rounded-lg border border-zinc-800  bg-zinc-900 shadow pr-80">
           <div className="flex-1">
-            <div className="w-full bg-zinc-950 aspect-video">
-              <ReactPlayer
-                width="100%"
-                height="100%"
-                controls
-                url="https://www.youtube.com/watch?v=HeMIZC2rkMo"
-              />
-            </div>
           </div>
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
             <div>
