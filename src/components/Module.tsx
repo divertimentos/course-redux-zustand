096da29cd7e8979c38f60c71f536b831cda6ec6b
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-19 19:08:59 -0300

    Starts implementing Redux through slices

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -2,6 +2,7 @@ import * as Collapsible from "@radix-ui/react-collapsible";
 
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
+import { useAppSelector } from "../store";
 
 interface ModuleProps {
   moduleIndex: number;
@@ -9,7 +10,17 @@ interface ModuleProps {
   amountOfLessons: number;
 }
 
+interface LessonInterface {
+  id: string;
+  title: string;
+  duration: string;
+}
+
 export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
+  const lessons = useAppSelector((state) => {
+    return state.player.course.modules[moduleIndex].lessons;
+  });
+
   return (
     <Collapsible.Root className="group">
       <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
@@ -25,10 +36,15 @@ export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          <Lesson title="Fundamentos do Redux" duration="9:13" />
-          <Lesson title="Fundamentos do Redux" duration="9:13" />
-          <Lesson title="Fundamentos do Redux" duration="9:13" />
-          <Lesson title="Fundamentos do Redux" duration="9:13" />
+          {lessons.map((lesson: LessonInterface) => {
+            return (
+              <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                duration={lesson.duration}
+              />
+            );
+          })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
