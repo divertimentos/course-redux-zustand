486642da6496cb887f80e7bb4aac7001924cd573
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-24 00:14:06 -0300

    WIP

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,20 +1,12 @@
 import ReactPlayer from "react-player";
 import { useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
-import { next } from "../store/slices/player";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
   const dispatch = useDispatch();
-  const lesson = useAppSelector((state) => {
-    const { currentModuleIndex, currentLessonIndex } = state.player;
 
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[
-        currentLessonIndex
-      ];
-
-    return currentLesson;
-  });
+  const { currentLesson } = useCurrentLesson();
 
   const handlePlayNext = () => {
     dispatch(next());
@@ -26,7 +18,7 @@ export function Video() {
         width="100%"
         height="100%"
         controls
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
         onEnded={handlePlayNext}
         // playing // <-- WARN: isto habilita o autoplay
       />
